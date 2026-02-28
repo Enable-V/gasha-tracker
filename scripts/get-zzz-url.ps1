dec4c7fd446bf6c7f42f41dd21578d5dbf51ffc0
Liht <alexey.hrushchyov@seoven.ru>
2026-02-28 22:23:29 +0300

    refactor: unify ZZZ script output style with HSR/Genshin

--- scripts/get-zzz-url.ps1
+++ scripts/get-zzz-url.ps1
@@ -1,20 +1,27 @@
 Add-Type -AssemblyName System.Web
 
-$logLocation = "%userprofile%\AppData\LocalLow\miHoYo\ZenlessZoneZero\Player.log";
-$logLocationAlt = "%userprofile%\AppData\LocalLow\Cognosphere\ZenlessZoneZero\Player.log";
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
 
-$apiHost = "public-operation-nap.hoyoverse.com"
+$logPaths = @(
+    "$env:USERPROFILE\AppData\LocalLow\miHoYo\ZenlessZoneZero\Player.log",
+    "$env:USERPROFILE\AppData\LocalLow\Cognosphere\ZenlessZoneZero\Player.log"
+)
 
 $tmps = $env:TEMP + '\pm.ps1';
 if ([System.IO.File]::Exists($tmps)) {
   ri $tmps
 }
 
-$path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
-if (-Not [System.IO.File]::Exists($path)) {
-    $path = [System.Environment]::ExpandEnvironmentVariables($logLocationAlt);
+$logFile = $null
+foreach ($path in $logPaths) {
+    if (Test-Path $path) {
+        $logFile = $path
+        break
+    }
 }
-if (-Not [System.IO.File]::Exists($path)) {
+
+if (-Not $logFile) {
     Write-Host "Cannot find the log file! Make sure to open the signal search history first!" -ForegroundColor Red
 
     if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -33,70 +40,101 @@ if (-Not [System.IO.File]::Exists($path)) {
     return
 }
 
-$logs = Get-Content -Path $path
-$m = $logs -match "(?m).:/.+ZenlessZoneZero_Data"
-$m[0] -match "(.:/.+ZenlessZoneZero_Data)" >$null
+$logContent = Get-Content $logFile -Raw
 
-if ($matches.Length -eq 0) {
+$gameDataPath = $null
+$patterns = @(
+    "([A-Z]:[/\\][^\n]*?ZenlessZoneZero_Data)"
+)
+
+foreach ($pattern in $patterns) {
+    $match = [regex]::Match($logContent, $pattern)
+    if ($match.Success) {
+        $gameDataPath = $match.Groups[1].Value -replace '/', '\'
+        break
+    }
+}
+
+if (-not $gameDataPath) {
     Write-Host "Cannot find the signal search url! Make sure to open the signal search history first!" -ForegroundColor Red
     return
 }
 
-$gamedir = $matches[1]
-$webcachePath = Resolve-Path "$gamedir/webCaches"
-$cacheVerPath = Get-Item (Get-ChildItem -Path $webcachePath | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
-$cachefile = Resolve-Path "$cacheVerPath/Cache/Cache_Data/data_2"
-$tmpfile = "$env:TEMP/zzz_data_2"
-
-Copy-Item $cachefile -Destination $tmpfile
-
-function testUrl($url) {
-  $ProgressPreference = 'SilentlyContinue'
-  $uri = [System.UriBuilder]::New($url)
-  $uri.Path = "common/gacha_record/api/getGachaLog"
-  $uri.Host = $apiHost
-  $uri.Fragment = ""
-  $params = [System.Web.HttpUtility]::ParseQueryString($uri.Query)
-  $params.Set("lang", "en");
-  $params.Set("gacha_type", 2);
-  $params.Set("real_gacha_type", 2);
-  $params.Set("size", "5");
-  $uri.Query = $params.ToString()
-  $apiUrl = $uri.Uri.AbsoluteUri
-
-  $response = Invoke-WebRequest -Uri $apiUrl -ContentType "application/json" -UseBasicParsing -TimeoutSec 10 | ConvertFrom-Json
-  $testResult = $response.retcode -eq 0
-  return $testResult
+$webCachePath = Join-Path $gameDataPath "webCaches"
+if (-not (Test-Path $webCachePath)) {
+    $webCachePath = Join-Path (Split-Path $gameDataPath) "webCaches"
 }
 
-$content = Get-Content -Encoding UTF8 -Raw $tmpfile
-$splitted = $content -split "1/0/"
-$found = $splitted -match "getGachaLog"
-$link = $false
+if (-not (Test-Path $webCachePath)) {
+    Write-Host "Cannot find the signal search url! Make sure to open the signal search history first!" -ForegroundColor Red
+    return
+}
+
+$cacheVersionDirs = Get-ChildItem -Path $webCachePath -Directory | Sort-Object Name -Descending
+$cacheFile = $null
+
+foreach ($dir in $cacheVersionDirs) {
+    $dataFile = Join-Path $dir.FullName "Cache\Cache_Data\data_2"
+    if (Test-Path $dataFile) {
+        $cacheFile = $dataFile
+        break
+    }
+}
+
+if (-not $cacheFile) {
+    Write-Host "Cannot find the signal search url! Make sure to open the signal search history first!" -ForegroundColor Red
+    return
+}
+
+$tmpFile = "$env:TEMP\zzz_data_2"
+Copy-Item $cacheFile -Destination $tmpFile -Force
+$cacheContent = [System.IO.File]::ReadAllText($tmpFile)
+
+$urlPattern = "https://[^\s\0]+(getGachaLog|gacha)[^\s\0]*authkey=[^\s\0]*"
+$foundMatches = [regex]::Matches($cacheContent, $urlPattern)
+
+if ($foundMatches.Count -eq 0) {
+    Write-Host "Cannot find the signal search url! Make sure to open the signal search history first!" -ForegroundColor Red
+    Remove-Item $tmpFile
+    return
+}
+
 $linkFound = $false
-for ($i = $found.Length - 1; $i -ge 0; $i -= 1) {
-  $t = $found[$i] -match "(https.+?end_id=)"
-  $link = $matches[0]
-  Write-Host "`rChecking Link $i" -NoNewline
-  $testResult = testUrl $link
-  if ($testResult -eq $true) {
-    $linkFound = $true
-    break
-  }
-  Sleep 1
+$cleanUrl = $null
+
+for ($i = $foundMatches.Count - 1; $i -ge 0; $i -= 1) {
+    $gachaUrl = $foundMatches[$i].Value
+    $cleanUrl = $gachaUrl -replace '["\s\0].*$', ''
+
+    Write-Host "`rChecking Link $i" -NoNewline
+
+    try {
+        $testUri = [System.Uri]$cleanUrl
+        $testHost = $testUri.Host
+        $testParams = [System.Web.HttpUtility]::ParseQueryString($testUri.Query)
+
+        $testUrl = "https://$testHost/common/gacha_record/api/getGachaLog?authkey_ver=$($testParams['authkey_ver'])&sign_type=$($testParams['sign_type'])&auth_appid=$($testParams['auth_appid'])&authkey=$([System.Uri]::EscapeDataString($testParams['authkey']))&game_biz=$($testParams['game_biz'])&lang=en&gacha_type=2&real_gacha_type=2&end_id=&page=1&size=5&region=$($testParams['region'])"
+        $response = Invoke-RestMethod -Uri $testUrl -Method Get -TimeoutSec 10
+
+        if ($response.retcode -eq 0) {
+            $linkFound = $true
+            break
+        }
+    } catch {
+        # continue to next link
+    }
+    Sleep 1
 }
 
-Remove-Item $tmpfile
+Remove-Item $tmpFile
 
 Write-Host ""
 
 if (-Not $linkFound) {
-  Write-Host "Cannot find the signal search url! Make sure to open the signal search history first!" -ForegroundColor Red
-  return
+    Write-Host "Cannot find the signal search url! Make sure to open the signal search history first!" -ForegroundColor Red
+    return
 }
 
-$wishHistoryUrl = $link
-
-Write-Host $wishHistoryUrl
-Set-Clipboard -Value $wishHistoryUrl
+Write-Host $cleanUrl
+Set-Clipboard -Value $cleanUrl
 Write-Host "Link copied to clipboard, paste it back to gacha-tracker.ru" -ForegroundColor Green
